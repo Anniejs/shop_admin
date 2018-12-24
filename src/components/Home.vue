52f08cab62f866a0f427b180283cb9953ec37032
Anniejs <anniejs@163.com>
2018-12-24 18:42:39 +0800

    初始化测试

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>这是home模块</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login">
+    <div class="login-box">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang='less'>
+.login{
+  width: 100%;
+  height: 100%;
+  background-color: #2D434C;
+  overflow: hidden;
+  .login-box{
+    width: 400px;
+    height: 300px;
+    background-color: #fff;
+    margin: 200px auto;
+
+  }
+}
+</style>
